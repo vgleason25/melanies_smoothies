89559254134959258ffb755252ab96ff6b48856e
vgleason25 <110790965+vgleason25@users.noreply.github.com>
2025-09-02 14:54:14 -0500

    Update streamlit_app.py
    change nutritional info to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,7 @@ if ingredients_list:
     #     st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.datframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
