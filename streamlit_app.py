92a50d03389850bd1035bf02a425475353149469
vgleason25 <110790965+vgleason25@users.noreply.github.com>
2025-09-03 13:31:40 -0500

    Update streamlit_app.py
    added space before for fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,12 @@ ingredients_list = st.multiselect (
     , max_selections=5
 )
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
     ingredients_string = ''
-    for fruit_chosen in ingredients_list:
+    
+   for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.datframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-    # for fruit_chosen in ingredients_list:
-    #     ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
