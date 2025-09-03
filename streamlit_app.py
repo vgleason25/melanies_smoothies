d0da7fa729d7fe9b8c3a7af93f627e4fdd9f0874
vgleason25 <110790965+vgleason25@users.noreply.github.com>
2025-09-03 13:35:49 -0500

    Update streamlit_app.py
    added a to dataframe on line 30ish

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.datframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
