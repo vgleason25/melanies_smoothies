5cf678ec6d099af133749e1e1eb0653914bcfaf3
vgleason25 <110790965+vgleason25@users.noreply.github.com>
2025-09-03 13:23:58 -0500

    Update streamlit_app.py
    got rid of #ed out row because row 58 was giving me trouble

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,6 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        # st.text(smoothiefroot_response.json()
         sf_df = st.datframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
